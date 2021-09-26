1b308a17f01804f8a6a00f7a92539c967d334ebe
stackxchen <stackxchen@tencent.com>
2021-09-26 11:51:17 +0800

    fix shell

--- git-commit.sh
+++ git-commit.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo $#
 if [ $# == 1 ]
 then
   git add .
   git commit -m "$1"
   git push origin dev
 else
-    echo "usage: git-commit.sh <commit-message>"
+    echo "usage: git-commit.sh \"commit-message\""
 fi
